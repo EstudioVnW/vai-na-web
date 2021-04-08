ce482b17ef359ac43478be9bbb92be06d7602fba
Evelyn <evelyn.laurindo@gmail.com>
2021-04-08 19:41:31 -0300

    inserted component footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from 'styled-components';
+
+// styles
+const Container = styled.footer`
+`;
+
+const Footer = () => {
+	return (
+		<Container>
+			<p>footer</p>
+		</Container>
+	)
+}
+
+export default Footer;
