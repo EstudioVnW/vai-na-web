afb4fa3264e1d41a826c33a55c90daa5cc8d06a6
Evelyn <evelyn.laurindo@gmail.com>
2021-04-19 16:35:09 -0300

    taking data from graphcms and adding env.production

--- src/components/blog/card.js
+++ src/components/blog/card.js
@@ -1,73 +1,88 @@
 import React from "react";
 import styled from 'styled-components';
+import { Link } from "gatsby";
 
 //Images
 import agenda from '../../images/images/agenda.svg';
 
 // styles
 const Container = styled.div`
+	padding-bottom: ${props => props.slider && '3.513rem'};
 	display: flex;
 	flex-direction:  ${props => props.slider && 'column'};
-	padding-bottom: ${props => props.slider && '56.2px'};
-	/* width: ${props => props.slider && '30%'}; */
+	align-items: center;
+	width: ${props => props.slider && '22.813rem'};
+	margin-right: 64px;
 `;
 
 const Image = styled.img`
-	width: ${props => props.slider ? '100%' : '693px'};
-
+	width: ${props => !props.slider && '50%'};
 `;
 
 const Content = styled.div`
 	padding-left: ${props => !props.slider && '2.813rem'};
-	width: ${props => !props.slider && '473px'};
+	width: ${props => !props.slider && '29.563rem'};
 `;
 
 const ContentDate = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
-	padding-top: ${props => props.slider && '24.64px'};
+	padding: ${props => props.slider ? '1.54rem 0 0.5rem 0' : '1.25rem 0'};
 `;
 
 const Date = styled.p`
 	font-size: 1rem;
 	color: #0F2B92;
+	font-family: "usual";
 `;
 
 const Status = styled.p`
-	padding: 0.5rem 1.188rem;
+	padding: 0 1.188rem;
+	height: 2.063rem;
 	font-size: 1rem;
 	color: #FF611E;
+	font-family: "usual";
 	text-transform: uppercase;
 	border: 1px solid #FF611E;
 	border-radius: 19px;
+	display: flex;
+	align-items: center;
 `;
 
-const Title = styled.h1`
-	padding: ${props => props.slider ? '8px 0 11px 0' : '1.25rem 0'};
-	font-size:  ${props => props.slider ? '30px' : '2.75rem'};
+const Title = styled(Link)`
+	font-size:  ${props => props.slider ? '1.875rem' : '2.75rem'};
 	font-weight: 700;
 	color: #272727;
-	font-family:"usual";
+	font-family: "usual";
+	text-decoration: none;
+	cursor: pointer;
+
+	:hover {
+		color: #0F2B92;
+		text-decoration: underline;
+	}
 `;
 
 const Description = styled.p`
+	padding-top: .688rem;
 	font-size: 1.125rem;
 	color: #272727;
+	font-family: "usual";
 	line-height: 1.75rem;
 `;
 
 const PostList = ({ data, slider }) => {
 	return (
 		<Container slider={slider}>
-			<Image src={agenda} alt='Agenda' slider={slider}/>
+		 <Image src={agenda} alt='Agenda' slider={slider}/>
 			<Content slider={slider}>
 				<ContentDate slider={slider}>
-					<Date>{data.date}</Date>
-					<Status>{data.status}</Status>
+					<Date>{data.publishedAt}</Date>
+					<Status>{data.tags && data?.tags[0].name}</Status>
 				</ContentDate>
-				<Title slider={slider}>{data.title}</Title>
-				<Description>{data.description}</Description>
+				<Title to={'/blog/postBlog/'} state={{ postBlog: data }} slider={slider}>{data.title}</Title>
+				<Description>{data.excerpt}</Description>
 			</Content>
 		</Container>
 	)
--- src/components/blog/postList.js
+++ src/components/blog/postList.js
@@ -1,27 +1,33 @@
+
 import React from "react";
+import { graphql } from "gatsby";
 import styled from 'styled-components';
 
+//Components
+import Layouts from '../../components/Layouts';
+import Card from '../../components/blog/card';
+
 // styles
-const Container = styled.section`
+const Container = styled.div`
+/* width: calc(70% / 3); */
+/* width: 33%; */
+
+/* display: flex;
+flex-wrap: wrap; */
+
+  display: grid;
+ column-gap: 77px;
+ grid-template-columns: repeat(3, 1fr);
+  padding: 44.2px 0 56.2px 0;
 `;
 
-const Content = styled.div`
-`;
-
-const  PostList = () => {
-	return (
+const PostList = ({ data }) => {
+  return (
 		<Container>
-			{/* <img src={} alt='' /> */}
-			<Content>
-				<div>
-					<p>data</p>
-					<p>tipo da noticia</p>
-				</div>
-				<h1>Titulo</h1>
-				<p>descrição</p>
-			</Content>
+			{data.posts.nodes.map(item => <Card data={item} slider/>)}
 		</Container>
-	)
+  )
 }
+
 
 export default PostList;
