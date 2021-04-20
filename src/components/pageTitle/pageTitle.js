ef4921d277c07025f5cc1f7220764d931317a373
Vai na Web <vainaweb@gmail.com>
2021-04-20 09:49:29 -0300

    Create componente pageTitle

---
+++ src/components/pageTitle/pageTitle.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from 'styled-components';
+
+
+// styles
+const Container = styled.section`
+  
+`;
+
+const PageTitle = () => {
+	return (
+		<Container>
+			<p>Ola</p>
+		</Container>
+	)
+}
+
+export default PageTitle;
