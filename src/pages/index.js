ab129b115b67536b20f824feeac02184679f500a
Kelvin Almeida <kelvin.f.almeida@gmail.com>
2021-05-12 16:34:58 -0300

    adjustment in the cases (desktop) section

--- src/pages/index.js
+++ src/pages/index.js
@@ -820,7 +820,7 @@ const Home = (props) => {
         {RenderNetwork()}  
         <PartnerSchols /> 
         <OverEstudio />
-        {/* {Cases()} */}
+        {Cases()}
         <Depositions />  
         <p>Blog</p>
         <History />
