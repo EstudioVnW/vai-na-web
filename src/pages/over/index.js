74dc54071cc2385b21c0b2df3334afa588a86989
Erlane <erlane.mendes31@gmail.com>
2021-05-13 14:45:42 -0300

    clear code

--- src/pages/over/index.js
+++ src/pages/over/index.js
@@ -218,11 +218,6 @@ const ContainerImages = styled.div`
   /* justify-content: space-around; */
   margin-top: 5rem;
 
-  /* @media (max-width: 768px) {
-    align-items: center;
-    flex-direction: column;
-	} */
-
   p {
     width: 45%;
     color: #00145D;
@@ -251,7 +246,6 @@ const ContainerImages = styled.div`
     }
 
     @media (max-width: 768px) {
-      /* align-items: center; */
       width: 100%;
       flex-direction: column;
 	  }
