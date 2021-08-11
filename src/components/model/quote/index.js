f29b96fb8ca568e1874ec7db8fe865a0cc62bc7d
liviavieira <vieira.liviaaa@gmail.com>
2021-08-11 19:04:09 -0300

    quote responsive

--- src/components/model/quote/index.js
+++ src/components/model/quote/index.js
@@ -3,6 +3,7 @@ import * as S from './styles';
 
 import QuoteProfile from '../../../images/images/QuoteProfile.png';
 import Line from '../../../images/images/Path8850.svg';
+import LineMobile from '../../../images/images/Path8889.svg'
 
 
 const ModelQuote = () => (
@@ -15,8 +16,9 @@ const ModelQuote = () => (
                     <i>As pessoas que transformarão nossos negócios, nossas economias e nosso país não necessariamente virão dos grandes centros ou das mais prestigiadas faculdades.</i> <strong>São as pessoas que conhecem a realidade das ruas, empoderadas pela tecnologia, que darão luz a uma nova onda de crescimento, inovação e produtividade.</strong>
                     <i> - Igor Couto, CEO 1STi</i>
                 </S.Text>
+                <S.QuoteLineMobile src={LineMobile} />
                 <S.BottomQuoteMark>“</S.BottomQuoteMark>
-                <S.BottomLine src={Line} />
+                <S.QuoteLine src={Line} />
             </S.TextBox>
         </S.Wrap>
     </S.QuoteContainer>
