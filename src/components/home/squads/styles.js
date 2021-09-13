e0a99bb14bd4a87b43b4ab88e5b3b9b6fde7cce2
Ana <80262469+liviavieira@users.noreply.github.com>
2021-09-13 18:19:12 -0300

    Update styles.js

--- src/components/home/squads/styles.js
+++ src/components/home/squads/styles.js
@@ -110,7 +110,7 @@ export const ButtonSquads = styled.div`
    justify-content: center;
    margin-top: -1.5rem;
    height: 3rem;
-   z-index: 111;
+   z-index: 9;
 `;
 
 export const ImageSquad = styled.img`
